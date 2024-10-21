0b8797f2b5285c8bab563e4d7c0a2532dcac149a
Pablo Rodriguez <103795541+RodriguezPabl@users.noreply.github.com>
2024-10-21 14:55:26 -0300

    ejercicio2 completo

---
+++ apl/ejercicio2/ejercicio2.cpp
@@ -0,0 +1,79 @@
+#include "ejercicio2.hpp"
+
+/**
+###                 INTEGRANTES                     ###
+###     Collazo, Ignacio Lahuel     - CONFIDENCE    ###
+###     Pozzato, Alejo Martin       - CONFIDENCE    ###
+###     Rodriguez, Emanual          - CONFIDENCE    ###
+###     Rodriguez, Pablo            - CONFIDENCE    ###
+###     Vazquez Petracca, Pablo N.  - CONFIDENCE    ###
+*/
+
+int main(int argc, char *argv[]) {
+    if (argc < 5) {
+        mostrarAyuda();
+        return 1;
+    }
+
+    string arg = argv[1];
+    if(arg == "-h" || arg == "--help") {
+        mostrarAyuda();
+        return 1;
+    }
+
+    string dirPath;
+    int numThreads = 0;
+    string cadenaABuscar;
+
+    for (int i = 1; i < argc; i++) {
+        arg = argv[i];
+        if (arg == "-d" || arg == "--directorio")
+            dirPath = argv[++i];
+        else
+        {
+            if (arg == "-t" || arg == "--threads")
+            {
+                numThreads = stoi(argv[++i]);
+                if (numThreads <= 0)
+                {
+                    cerr << "Error: El número de hilos debe ser un entero positivo.\n";
+                    return 1;
+                }
+            }
+            else
+                cadenaABuscar = arg;
+        }
+    }
+
+    if (dirPath.empty())
+    {
+        cerr << "Error: El directorio es un parametro obligatorio.\n";
+        return 1;
+    }
+
+    signal(SIGINT, signalHandler);
+    signal(SIGTERM, signalHandler);
+
+    for (const auto &entry : filesystem::directory_iterator(dirPath)) {
+        if (entry.is_regular_file()) {
+            fileQueue.push(entry.path().string());
+        }
+    }
+
+    vector<thread> threads;
+    for (int i = 0; i < numThreads; ++i) {
+        threads.emplace_back(trabajo, i + 1, cadenaABuscar);
+    }
+
+    {
+        lock_guard<mutex> lock(mtx);
+        done = true;
+    }
+    cv.notify_all();
+
+    for (auto &thread : threads) {
+        thread.join();
+    }
+
+    return 0;
+}
---
+++ apl/ejercicio2/ejercicio2.hpp
@@ -0,0 +1,81 @@
+#ifndef EJERCICIO2_HPP_INCLUDED
+#define EJERCICIO2_HPP_INCLUDED
+
+/**
+###                 INTEGRANTES                     ###
+###     Collazo, Ignacio Lahuel     - CONFIDENCE    ###
+###     Pozzato, Alejo Martin       - CONFIDENCE    ###
+###     Rodriguez, Emanual          - CONFIDENCE    ###
+###     Rodriguez, Pablo            - CONFIDENCE    ###
+###     Vazquez Petracca, Pablo N.  - CONFIDENCE    ###
+*/
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <csignal>
+
+using namespace std;
+
+std::mutex mtx;
+std::condition_variable cv;
+std::queue<std::string> fileQueue;
+bool done = false;
+
+void buscarEnArchivo(const string &nombreArchivo, const string &cadenaABuscar, int threadId) {
+    ifstream file(nombreArchivo);
+    if (!file.is_open()) {
+        cerr << "Error: No se pudo abrir el archivo " << nombreArchivo << "\n";
+        return;
+    }
+
+    string line;
+    int lineNumber = 0;
+    while (getline(file, line)) {
+        lineNumber++;
+        if (line.find(cadenaABuscar) != string::npos) {
+            lock_guard<mutex> lock(mtx);
+            cout << "Nro de Thread: " << threadId << " - El nombre del archivo: "
+                      << nombreArchivo << " - El numero de línea: " << lineNumber << endl;
+        }
+    }
+}
+
+void trabajo(int threadId, const string &cadenaABuscar) {
+    while (true) {
+        string nombreArchivo;
+
+        {
+            unique_lock<mutex> lock(mtx);
+            cv.wait(lock, [] { return !fileQueue.empty() || done; });
+
+            if (done && fileQueue.empty()) break;
+
+            nombreArchivo = fileQueue.front();
+            fileQueue.pop();
+        }
+
+        buscarEnArchivo(nombreArchivo, cadenaABuscar, threadId);
+    }
+}
+
+void signalHandler(int signal) {
+    done = true;
+    cv.notify_all();
+}
+
+void mostrarAyuda() {
+    cout << "Uso: ./program -d <path> -t <nro> <cadena_a_buscar> [-h]\n"
+              << "Opciones:\n"
+              << "  -d / --directorio <path>   Ruta del directorio a analizar (Requerido)\n"
+              << "  -t / --threads <nro>       Cantidad de threads a ejecutar concurrentemente (Requerido)\n"
+              << "  -h / --help                 Muestra esta ayuda\n";
+}
+
+#endif // EJERCICIO2_HPP_INCLUDED
